394a168915eccdc5a611fb8c47e2b77d74133190
sylvernus akubo <akubosylvernus@gmail.com>
2022-04-08 09:11:03 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import "./App.css";
-import { useRoutes } from "react-router-dom";
-import { RouteEnum } from "common/constants";
-import { Suspense } from "react";
-import Home from "home/Home";
-import AppThemeProvider from "AppThemeProvider";
+import './App.css';
+import {useRoutes} from 'react-router-dom';
+import {RouteEnum} from 'common/constants';
+import {Suspense} from 'react';
+import Home from 'home/Home';
+import AppThemeProvider from 'AppThemeProvider';
+import Products from 'products/Products';
+import 'assets/slick/slick.css';
+import 'assets/slick/slick-theme.css';
 
-function App() {
-  const ROUTES = useRoutes([
+function App () {
+  const ROUTES = useRoutes ([
+    {
+      path: RouteEnum.PRODUCTS,
+      element: <Products />,
+    },
     {
       path: RouteEnum.HOME,
       element: <Home />,
     },
     {
-      path: "*",
+      path: '*',
       element: <Home />,
     },
   ]);
 
   return (
     <AppThemeProvider>
-      <Suspense fallback={"loading..."}>{ROUTES}</Suspense>
+      <Suspense fallback={'loading...'}>{ROUTES}</Suspense>
     </AppThemeProvider>
   );
 }
---
+++ src/products/Products.js
@@ -0,0 +1,71 @@
+import {Button, Rating, Typography} from '@mui/material';
+import ContainerSection from 'common/ContainerSection';
+import React from 'react';
+import ProductDetailSlider from './ProductDetailSlider';
+
+export default function Products () {
+  return (
+    <ContainerSection>
+      <header className=" w-full min-h-screen relative flex justify-center items-center">
+
+        <div className="container grid grid-cols-1 md:grid-cols-2 gap-5 items-center py-[100px] md:flex-row flex-col z-10 px-5 md:px-2">
+          <div>
+            <div className="max-h-lg">
+              <ProductDetailSlider products={PRODUCTS_IMAGE_ARRAY} />
+            </div>
+          </div>
+
+          <div className="flex justify-center">
+            <div className="max-w-md">
+              <Typography variant="h3">
+                Restaurant Delivery Robot System
+              </Typography>
+
+              <div className='flex gap-2 items-end'>
+              <Rating size='small' name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
+              <Typography variant='body2' className='text-gray-400'> (11.78kreviews)</Typography>
+              </div>
+
+              <div className="mt-5 flex gap-2 ">
+                <Typography variant="h5" className="text-gray-400 mr-2">
+                  $24,943
+                </Typography>
+                {' '}
+                <Typography variant="h5">$24,943</Typography>
+              </div>
+
+              <div className='mt-8 flex flex-col gap-5'>
+                <div className='flex justify-between gap-2 items-center'>
+                  <Typography variant='subtitle1'>Color</Typography>
+                  <></>
+                </div>
+                <div className='flex justify-between gap-2 items-center'>
+                  <Typography variant='subtitle1'>Size</Typography>
+                  <></>
+                </div>
+                <div className='flex justify-between gap-2 items-center'>
+                  <Typography variant='subtitle1'>Quantity</Typography>
+                  <></>
+                </div>
+              </div>
+
+              <div className='grid grid-cols-3 gap-3 mt-10'>
+                <Button variant="contained" size='large' className='bg-green-600 font-semibold' fullWidth>Added To Cart</Button>
+                <Button variant="contained" className='col-span-2 font-semibold' size='large' fullWidth>Added To Cart</Button>
+              </div>
+
+              <Button variant="contained" size='large' className='text-black bg-gray-300 mt-5 font-semibold' fullWidth>Added To Cart</Button>
+            </div>
+          </div>
+        </div>
+      </header>
+    </ContainerSection>
+  );
+}
+
+const PRODUCTS_IMAGE_ARRAY = [
+  'https://res.cloudinary.com/fainzy-technologies/image/upload/v1649235689/headerImage_coel8n.png',
+  'https://res.cloudinary.com/fainzy-technologies/image/upload/v1649197229/Layer_0_2_h8xz0d.png',
+  'https://res.cloudinary.com/fainzy-technologies/image/upload/v1649235689/headerImage_coel8n.png',
+  'https://res.cloudinary.com/fainzy-technologies/image/upload/v1649197108/restuarant_robot_1_1_ds6125.png',
+];
