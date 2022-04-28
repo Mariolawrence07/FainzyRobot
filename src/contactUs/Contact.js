e40be9d2388fd435a97b4ac5ddf1a12fbfba0b35
Lawrence Obidike <obidikelawrence@yahoo.com>
2022-04-28 09:45:29 -0400

    removed card action fron contact

--- src/contactUs/Contact.js
+++ src/contactUs/Contact.js
@@ -2,7 +2,7 @@ import { TextField, Typography } from "@mui/material";
 import ContainerSection from "common/ContainerSection";
 import React from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
+
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 
